9812481f08b1e10528a21c4f660538b060cf6c46
Abhishek-Saharn <abhishek.saharn@gmail.com>
2018-02-26 23:16:53 +0530

    commit first

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ console.log("server Connected");
 var playersCount = 0;
 
 io.on('connection', function(socket) {
-    console.log(socket);
+
     console.log("Client Connected");
     playersCount++;
     socket.broadcast.emit("spawn");
@@ -15,7 +15,8 @@ io.on('connection', function(socket) {
     }
 
     socket.on('move', function(data) {
-        console.log('Client Moved with Data:' + data);
+        console.log('Client played Moved with Data:');
+        console.log('Client played Moved with Data:' + JSON.stringify(data));
     });
 
     socket.on('disconnect', function() {
